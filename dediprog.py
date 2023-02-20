1b1daafb8521a0fdf786d908d57078dacc814ed5
Chen Eason (IFTW PSS SMD GC TM DCO) <Eason.Chen@infineon.com>
2023-02-20 13:30:03 +0800

    fix no match case

--- dediprog.py
+++ dediprog.py
@@ -10,7 +10,7 @@ def get_dediprog_html(part_number):
              #        'ddlProgrammer[]':'81',
              #        'ddlProgrammer[]':'21',
              #        'ddlProgrammer[]':'26',
-             #'ddlManuf':'767',       # ddlManuf=767 means Infineon
+             # 'ddlManuf':'767',       # ddlManuf=767 means Infineon
              "per_page_50": "100",
              }
     r = requests.get(cs_url, params=param)
@@ -21,31 +21,31 @@ def get_dediprog_html(part_number):
     soup = bs(html, "html.parser")
     # print(soup)
 
-
-
-
     soup = bs(html, "html.parser")
     spans = soup.find_all('font', {'size': "-1"})
 
     i = 0
-    result_part_number_list=list()
+    result_part_number_list = list()
     for index, s in enumerate(spans):
         if index == 7+i*6:
             result_part_number_list.append(s.string)
             i += 1
 
-    new_list=result_part_number_list[0:-1]
+    new_list = result_part_number_list[0:-1]
 
-    final_result_part_number_list=list()
+    final_result_part_number_list = list()
     for s in new_list:
-        if s  != None:
+        if s != None:
             final_result_part_number_list.append(s[2:])
+
+    if len(final_result_part_number_list) == 0:
+        final_result_part_number_list.append("No match")
 
     return final_result_part_number_list
 
 
 if __name__ == "__main__":
 
-    part_list=get_dediprog_html('xdpe192c')
+    part_list = get_dediprog_html('xdpe192c')
 
     print(part_list)
--- hilo.py
+++ hilo.py
@@ -2,48 +2,52 @@ import urllib.request as req
 from urllib import parse
 import json
 
+
 def get_hilo_html(part_number):
     url = 'https://www.hilosystems.com/search/ajaxLoadData'
-    data = parse.urlencode({"ic_name":part_number}).encode()
-    request=req.Request(url,
-                        data=data,
-                        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
-                                       })
-
+    data = parse.urlencode({"ic_name": part_number}).encode()
+    request = req.Request(url,
+                          data=data,
+                          headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
+                                   })
 
     with req.urlopen(request) as response:
-        data=response.read().decode("utf-8")
+        data = response.read().decode("utf-8")
 
-    json_data=json.loads(data)
+    json_data = json.loads(data)
 
-    result_list=list()
+    result_list = list()
 
-    for i in range(0,len(json_data['data']['data_array'])):
+    for i in range(0, len(json_data['data']['data_array'])):
         result_list.append(json_data['data']['data_array'][i]['I_IC'])
 
+    if len(result_list) == 0:
+        result_list.append("No match")
+
     return result_list
+
 
 def get_acroview_result(part_number):
-    url='https://www.acroview.com/support/_list/num/20/page/1'
-    data = parse.urlencode({"chip":part_number}).encode()
-    request=req.Request(url,
-                        data=data,
-                        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
-                                       })
+    url = 'https://www.acroview.com/support/_list/num/20/page/1'
+    data = parse.urlencode({"chip": part_number}).encode()
+    request = req.Request(url,
+                          data=data,
+                          headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
+                                   })
     with req.urlopen(request) as response:
-        data=response.read().decode("utf-8")
+        data = response.read().decode("utf-8")
 
-    json_data=json.loads(data)
-    result_list=list()
-    for i in range(0,len(json_data['data'])):
-        result_list.append(json_data['data'][i]['chip'])
-    
+    json_data = json.loads(data)
+    result_list = list()
+    try:
+        for i in range(0, len(json_data['data'])):
+            result_list.append(json_data['data'][i]['chip'])
+    except:
+        result_list.append("No match")
+
     return result_list
-    
-    
+
+
 if __name__ == "__main__":
 
-    data=get_acroview_result('xdpe1')
-    
-
-    
+    data = get_acroview_result('xdpe1')
